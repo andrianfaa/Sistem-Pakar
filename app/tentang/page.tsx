81fa814b4c57059ff850a53f18f0654d29134774
andrianfaa <hey@anfa.my.id>
2026-01-19 21:39:38 +0700

    feat: add anggotaKelompok data and update AboutPage to display team members

--- app/tentang/page.tsx
+++ app/tentang/page.tsx
@@ -1,6 +1,7 @@
+import anggotaKelompok from "@/kelompok";
 import Image from "next/image";
 import Link from "next/link";
-// import { FaUser } from "react-icons/fa";
+import { FaUser } from "react-icons/fa";
 
 export default function AboutPage() {
   return (
@@ -17,32 +18,39 @@ export default function AboutPage() {
       <p className="mb-3">Sistem ini dibangun dan dikembangkan oleh:</p>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 md:grid-cols-3">
-        <div className="p-4 border border-gray-300 shadow-sm rounded-lg">
-          {/* <div className="rounded-full w-20 h-20 flex items-center justify-center bg-gray-200/50 mx-auto">
-            <FaUser className="size-8 text-heading" />
-          </div> */}
-          {/* Rounded image */}
-          <Image
-            src={"https://avatars.githubusercontent.com/u/74356783?v=4"}
-            alt="Andrian Fadhilla"
-            width={80}
-            height={80}
-            className="rounded-full mx-auto"
-          />
+        {anggotaKelompok.map((anggota) => (
+          <div key={anggota.npm} className="p-4 border border-gray-300 shadow-sm rounded-lg">
+            {anggota.website ? (
+              <Image
+                src={"https://avatars.githubusercontent.com/u/74356783?v=4"}
+                alt="Andrian Fadhilla"
+                width={80}
+                height={80}
+                className="rounded-full mx-auto"
+              />
+            ) : (
+              <div className="rounded-full w-20 h-20 flex items-center justify-center bg-gray-200/50 mx-auto">
+                <FaUser className="size-8 text-heading" />
+              </div>
+            )}
 
-          <div className="mt-4 text-center">
-            <h3 className="text-lg/tight font-semibold text-heading">Andrian Fadhilla</h3>
+            <div className="mt-4 text-center">
+              <h3 className="text-lg/tight font-semibold text-heading">{anggota.nama}</h3>
 
-            <p className="mt-0.5 text-sm">109220640085</p>
-            <p className="mt-3 text-pretty">Mahasiswa Teknik Informatika di STMIK Pranata Indonesia.</p>
-            <Link
-              href="https://www.anfa.my.id"
-              className="mt-4 inline-block text-sm font-semibold text-primary hover:underline"
-            >
-              www.anfa.my.id
-            </Link>
+              <p className="mt-0.5 text-sm">{anggota.npm}</p>
+              <p className="mt-3 text-pretty">Mahasiswa Teknik Informatika di STMIK Pranata Indonesia.</p>
+
+              {anggota.website && (
+                <Link
+                  href="https://www.anfa.my.id"
+                  className="mt-4 inline-block text-sm font-semibold text-primary hover:underline"
+                >
+                  www.anfa.my.id
+                </Link>
+              )}
+            </div>
           </div>
-        </div>
+        ))}
       </div>
     </div>
   );
---
+++ kelompok.ts
@@ -0,0 +1,17 @@
+const anggotaKelompok = [
+  {
+    nama: "Andrian Fadhilla",
+    npm: "109220640085",
+    website: "https://www.anfa.my.id"
+  },
+  {
+    nama: "M. Fajar Julianto",
+    npm: "109220640028"
+  },
+  {
+    nama: "Choliq Almahdi",
+    npm: "109220640012"
+  }
+];
+
+export default anggotaKelompok;
