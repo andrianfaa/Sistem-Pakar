a4d15712934984fc6c558e71da9f594509798ef4
andrianfaa <hey@anfa.my.id>
2026-01-19 22:05:05 +0700

    fix: remove default baseURL in redaxios instance and log current baseURL

--- libs/redaxios.ts
+++ libs/redaxios.ts
@@ -1,7 +1,9 @@
 import redaxios from "redaxios";
 
+console.log("Base URL:", process.env.BASE_URL || "not defined");
+
 const instance = redaxios.create({
-  baseURL: process.env.BASE_URL || "http://localhost:3000",
+  baseURL: process.env.BASE_URL,
   headers: {
     "Content-Type": "application/json"
   }
